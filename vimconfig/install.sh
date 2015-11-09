30089352a6203fe28febc721c0a3bee66a94deb7
zc <i.mzc@qq.com>
2015-11-09 20:36:56 +0800

    add vundle

--- vimconfig/install.sh
+++ vimconfig/install.sh
@@ -8,7 +8,7 @@ mv ~/.vim backup/`date +%Y%m%d`.vim  1>/dev/null 2>&1
 
 #tar zxf ./vimconf.tgz  -C ~/
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
 cp -R .vim ~/.vim 
 #git submodule foreach git submodule update
 cp -R .vimrc ~/.vimrc
