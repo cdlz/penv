e1962ed77a8d86025bb5d653c1dbea725f6c04a3
zc <i.mzc@qq.com>
2015-11-09 18:02:47 +0800

    add git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim

--- vimconfig/install.sh
+++ vimconfig/install.sh
@@ -7,6 +7,8 @@ mv ~/.vimrc backup/`date +%Y%m%d`.vimrc 1>/dev/null 2>&1
 mv ~/.vim backup/`date +%Y%m%d`.vim  1>/dev/null 2>&1
 
 #tar zxf ./vimconf.tgz  -C ~/
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp -R .vim ~/.vim
 cp -R .vimrc ~/.vimrc
 
