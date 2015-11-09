6891d757f6b353b4cea5eeb1fa8b400d048a9513
zc <i.mzc@qq.com>
2015-11-09 18:04:53 +0800

    git submodule foreach git submodule update

--- vimconfig/install.sh
+++ vimconfig/install.sh
@@ -9,7 +9,8 @@ mv ~/.vim backup/`date +%Y%m%d`.vim  1>/dev/null 2>&1
 #tar zxf ./vimconf.tgz  -C ~/
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp -R .vim ~/.vim
+cp -R .vim ~/.vim 
+#git submodule foreach git submodule update
 cp -R .vimrc ~/.vimrc
 
 echo "finished[use BundleInstall for ensure the bundles had been installed successfully]"
